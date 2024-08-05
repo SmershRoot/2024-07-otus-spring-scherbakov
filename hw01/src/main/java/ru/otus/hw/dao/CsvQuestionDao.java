060ab0d483f9119bc2297afee45b6a09d958e95e
Щербаков Александр <Smersh.Root@gmail.com>
2024-08-05 22:07:08 +0500

    hw01: fix: CsvQuestionDao. inputStream clouse.  Переделал исключения

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -7,6 +7,7 @@ import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
@@ -18,11 +19,10 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        var inputStream = getClass().getResourceAsStream(fileNameProvider.getTestFileName());
-        if (Objects.isNull(inputStream)) {
-            throw new QuestionReadException("Источник получения данных не доступен");
-        }
-        try {
+        try (var inputStream = getClass().getResourceAsStream(fileNameProvider.getTestFileName())) {
+            if (Objects.isNull(inputStream)) {
+                throw new QuestionReadException("Источник получения данных не доступен");
+            }
             Reader reader = new InputStreamReader(inputStream);
             var questionDtos = new CsvToBeanBuilder<QuestionDto>(reader)
                     .withType(QuestionDto.class)
@@ -31,6 +31,10 @@ public class CsvQuestionDao implements QuestionDao {
                     .build()
                     .parse();
             return questionDtos.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (QuestionReadException e) {
+            throw new QuestionReadException(e.getMessage());
+        } catch (IOException e) {
+            throw new QuestionReadException("Ошибка взаимодействия с источником данных", e);
         } catch (Exception e) {
             throw new QuestionReadException("Ошибка чтения данных", e);
         }
