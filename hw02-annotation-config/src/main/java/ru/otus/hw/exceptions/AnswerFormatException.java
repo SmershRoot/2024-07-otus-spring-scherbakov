4bae71526476a5855ada8a94e5d33e0e717fa6f8
Щербаков Александр <Smersh.Root@gmail.com>
2024-08-19 00:06:04 +0500

    fix: Проверки осуществляю на этапе ввода

--- hw02-annotation-config/src/main/java/ru/otus/hw/exceptions/AnswerFormatException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw.exceptions;
-
-public class AnswerFormatException  extends RuntimeException {
-    public AnswerFormatException(String message) {
-        super(message);
-    }
-}
--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -8,10 +8,8 @@ import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.TestResult;
-import ru.otus.hw.exceptions.AnswerFormatException;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -24,9 +22,6 @@ public class TestServiceImpl implements TestService {
 
     private static final String TEXT_ERROR_INPUT_ANSWER_NUMBER = TEXT_ERROR_INPUT_ANSWER
             + " (answer numbers separated by space or comma)";
-
-    private static final String TEXT_ERROR_INPUT_ANSWER_INDEX = TEXT_ERROR_INPUT_ANSWER
-            + " The answer option must be within the answer numbers";
 
     private final IOService ioService;
 
@@ -48,14 +43,11 @@ public class TestServiceImpl implements TestService {
     }
 
     private boolean askQuestion(int numberQuestion, Question question) {
-        try {
-            printQuestion(numberQuestion, question);
-            var studentAnswer = ioService.readStringWithPrompt(TEXT_FOR_ANSWER + ":");
-            return checkAnswer(question, studentAnswer);
-        } catch (AnswerFormatException e) {
-            ioService.printFormattedLine(e.getMessage() + "%n");
-            return askQuestion(numberQuestion, question);
-        }
+        printQuestion(numberQuestion, question);
+        var studentAnswer = ioService.readIntListByForRangeWithPrompt(
+                1, question.answers().size(),
+                TEXT_FOR_ANSWER, TEXT_ERROR_INPUT_ANSWER_NUMBER);
+        return checkAnswer(question, studentAnswer);
     }
 
     private void printQuestion(int numberQuestion, Question question) {
@@ -71,31 +63,14 @@ public class TestServiceImpl implements TestService {
         }
     }
 
-    private boolean checkAnswer(Question question, String studentAnswer) {
-        List<Integer> choices = splitAnswer(studentAnswer);
+    private boolean checkAnswer(Question question, List<Integer> studentAnswer) {
         var questionAnswers = question.answers();
-        for (var choice: choices) {
-            if (choice < 1 || choice > questionAnswers.size()) {
-                throw new AnswerFormatException(TEXT_ERROR_INPUT_ANSWER_INDEX);
-            }
+        for (var choice: studentAnswer) {
             if (!questionAnswers.get(choice - 1).isCorrect()) {
                 return false;
             }
         }
         return true;
-    }
-
-    private List<Integer> splitAnswer(String answer) {
-        try {
-            var options = Stream.of(answer.trim().split("[,\\s]+"))
-                    .filter(option -> !option.isEmpty()).map(Integer::parseInt).toList();
-            if (options.isEmpty()) {
-                throw new AnswerFormatException(TEXT_ERROR_INPUT_ANSWER_NUMBER);
-            }
-            return options;
-        } catch (NumberFormatException e) {
-            throw new AnswerFormatException(TEXT_ERROR_INPUT_ANSWER_NUMBER);
-        }
     }
 
 }
