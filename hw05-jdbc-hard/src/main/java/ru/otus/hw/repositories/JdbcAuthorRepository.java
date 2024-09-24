b940262e7526046ca3734f90120bc2211a759445
Щербаков Александр <Smersh.Root@gmail.com>
2024-09-24 23:22:58 +0500

    hw05: Выполнение ДЗ

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,51 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        return namedParameterJdbcTemplate.query("SELECT id, full_name FROM authors", new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+        var params = Map.of("id", id);
+        var authors = namedParameterJdbcTemplate.query(
+                "SELECT id, full_name FROM authors WHERE id = :id",
+                params, new AuthorRowMapper()
+        );
+
+        if (authors.size() > 1) {
+            throw new RuntimeException("More than one author found");
+        }
+
+        return authors.isEmpty() ? Optional.empty() : Optional.of(authors.get(0));
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            var id = rs.getLong("id");
+            var fullName = rs.getString("full_name");
+
+            return new Author(id, fullName);
         }
     }
 }
--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -6,28 +6,39 @@ import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcGenreRepository implements GenreRepository {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public List<Genre> findAll() {
-        return new ArrayList<>();
+        return namedParameterJdbcTemplate.query("SELECT id, name FROM genres", new GnreRowMapper());
     }
 
     @Override
     public List<Genre> findAllByIds(Set<Long> ids) {
-        return new ArrayList<>();
+        var params = Map.of("ids", ids);
+        return namedParameterJdbcTemplate.query("SELECT id, name FROM genres WHERE id IN (:ids)",
+                params,
+                new GnreRowMapper());
     }
 
     private static class GnreRowMapper implements RowMapper<Genre> {
 
         @Override
         public Genre mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Genre(
+                    rs.getLong("id"),
+                    rs.getString("name")
+            );
         }
     }
 }
