843e6d2e6bb473a99d82f6a3db89b41a68fea42e
Щербаков Александр <Smersh.Root@gmail.com>
2024-11-18 13:23:01 +0500

    hw09: init проект на основе hw07

---
+++ hw09/src/main/java/ru/otus/hw/mapper/BookMapper.java
@@ -0,0 +1,14 @@
+package ru.otus.hw.mapper;
+
+import org.mapstruct.Mapper;
+import ru.otus.hw.dto.BookDTO;
+import ru.otus.hw.models.Book;
+
+@Mapper(componentModel = "spring")
+public interface BookMapper {
+
+    BookDTO toBookDTO(Book book);
+
+    Book toBook(BookDTO bookDTO);
+
+}
