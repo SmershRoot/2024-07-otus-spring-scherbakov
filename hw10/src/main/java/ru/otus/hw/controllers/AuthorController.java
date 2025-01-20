16e87df91fa3e0080a9e7f80420ee3955948f5b7
Щербаков Александр <Smersh.Root@gmail.com>
2025-01-20 10:57:08 +0500

    hw10: fix codestyle

--- hw10/src/main/java/ru/otus/hw/controllers/AuthorController.java
+++ hw10/src/main/java/ru/otus/hw/controllers/AuthorController.java
@@ -1,9 +1,7 @@
 package ru.otus.hw.controllers;
 
 import java.util.List;
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw.dto.AuthorDTO;
@@ -11,13 +9,12 @@ import ru.otus.hw.services.AuthorService;
 
 @RestController
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthorController {
 
-    AuthorService service;
+    private final AuthorService service;
 
     @GetMapping("/author")
-    public List<AuthorDTO> read(){
+    public List<AuthorDTO> read() {
         return service.findAll();
     }
 
--- hw10/src/main/java/ru/otus/hw/services/BookService.java
+++ hw10/src/main/java/ru/otus/hw/services/BookService.java
@@ -4,8 +4,6 @@ import ru.otus.hw.dto.BookDTO;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 public interface BookService {
     Book findById(long id);
