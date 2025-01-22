f2ae3fc5d52969a4d1750b4675290d06fead45bd
Щербаков Александр <Smersh.Root@gmail.com>
2025-01-22 10:21:15 +0500

    hw14: Добавил restart через JobOperator

--- hw14/src/main/java/ru/otus/hw/commands/BatchMigrationCommands.java
+++ hw14/src/main/java/ru/otus/hw/commands/BatchMigrationCommands.java
@@ -5,10 +5,14 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 import org.springframework.batch.core.Job;
+
+import java.time.LocalDateTime;
+import java.util.Properties;
 
 import static ru.otus.hw.configuration.migration.mono.to.h2.BatchConfigurationMongoToH2.IMPORT_JOB_NAME;
 
@@ -20,12 +24,22 @@ public class BatchMigrationCommands {
 
     private final JobLauncher jobLauncher;
 
+    private final JobOperator jobOperator;
+
     private final JobExplorer jobExplorer;
 
     @ShellMethod(value = "startMigrationJobWithJobLauncherMongoToH2", key = "sm-m-2h")
     public void startMigrationJobWithJobLauncherMongoToH2() throws Exception {
         JobExecution execution = jobLauncher.run(migrateJobMongoToH2, new JobParameters());
         System.out.println(execution);
+    }
+
+    @ShellMethod(value = "restartMigrationJobWithJobLauncherMongoToH2", key = "sm-m-2h-restart")
+    public void restartMigrationJobWithJobLauncherMongoToH2() throws Exception {
+        Properties properties = new Properties();
+        properties.put("dataTimeRestart", LocalDateTime.now().toString());
+        Long executionId = jobOperator.start(IMPORT_JOB_NAME, properties);
+        System.out.println(jobOperator.getSummary(executionId));
     }
 
     @SuppressWarnings("unused")
