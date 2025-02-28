03b54d8034b62854be13647d0f30eb56aa70aaab
Щербаков Александр <Smersh.Root@gmail.com>
2025-03-01 00:56:05 +0500

    hw16: hal and data rest

--- hw16/src/main/java/ru/otus/hw/Application.java
+++ hw16/src/main/java/ru/otus/hw/Application.java
@@ -11,6 +11,8 @@ public class Application {
 		System.out.printf("Серверная часть: %n%s%n", "http://localhost:8080/library-api/swagger-ui/index.html");
 		System.out.printf("Actuator: %n%s%n", "http://localhost:8080/library-api/actuator");
 		System.out.printf("Prometheus: %n%s%n", "http://localhost:8080/library-api/actuator/prometheus");
+		System.out.printf("HAL explorer: %n%s%n", "http://localhost:8080/library-api/explorer");
+		System.out.printf("Data REST: %n%s%n", "http://localhost:8080/library-api/data-rest/book ({entity-name} book for example)");
 	}
 
 }
--- hw16/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -1,15 +1,15 @@
 package ru.otus.hw.repositories;
 
-//import jakarta.annotation.Nonnull;
-//import jakarta.validation.constraints.NotNull;
 import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
 import java.util.Optional;
 
+@RepositoryRestResource(path = "book")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Override
