58f36abfa755ad8136de4938fd8b46324349ca1b
Щербаков Александр <Smersh.Root@gmail.com>
2025-03-01 12:23:01 +0500

    Revert "hw16: добавил excerptProjection для Book"
    This reverts commit 58eadd210437231aa64bb3f4babf19ed88457ab0.

--- hw16/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Book;
-import ru.otus.hw.projections.BookAuthorProjection;
 
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource(path = "book", excerptProjection = BookAuthorProjection.class)
+@RepositoryRestResource(path = "book")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Override
