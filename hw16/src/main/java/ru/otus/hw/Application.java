58eadd210437231aa64bb3f4babf19ed88457ab0
Щербаков Александр <Smersh.Root@gmail.com>
2025-03-01 11:33:16 +0500

    hw16: добавил excerptProjection для Book

--- hw16/src/main/java/ru/otus/hw/Application.java
+++ hw16/src/main/java/ru/otus/hw/Application.java
@@ -11,7 +11,7 @@ public class Application {
 		System.out.printf("Серверная часть: %n%s%n", "http://localhost:8080/library-api/swagger-ui/index.html");
 		System.out.printf("Actuator: %n%s%n", "http://localhost:8080/library-api/actuator");
 		System.out.printf("Prometheus: %n%s%n", "http://localhost:8080/library-api/actuator/prometheus");
-		System.out.printf("HAL explorer: %n%s%n", "http://localhost:8080/library-api/explorer");
+		System.out.printf("HAL explorer: %n%s%n", "http://localhost:8080/library-api/data-rest/explorer");
 		System.out.printf("Data REST: %n%s%n", "http://localhost:8080/library-api/data-rest/book ({entity-name} book for example)");
 	}
 
--- hw16/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Book;
+import ru.otus.hw.projections.BookAuthorProjection;
 
 import java.util.List;
 import java.util.Optional;
 
-@RepositoryRestResource(path = "book")
+@RepositoryRestResource(path = "book", excerptProjection = BookAuthorProjection.class)
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Override
