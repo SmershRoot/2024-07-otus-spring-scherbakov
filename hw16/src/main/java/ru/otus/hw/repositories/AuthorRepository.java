f4d8f0aafd195f64631cab6bcd81714a47264b07
Щербаков Александр <Smersh.Root@gmail.com>
2025-03-01 12:23:10 +0500

    Revert "hw16: hal and data rest"
    This reverts commit 03b54d8034b62854be13647d0f30eb56aa70aaab.

--- hw16/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
@@ -1,12 +1,10 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.hw.models.Author;
 
 import java.util.List;
 
-@RepositoryRestResource(path = "author")
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
     List<Author> findByIdNotIn(List<Long> ids);
